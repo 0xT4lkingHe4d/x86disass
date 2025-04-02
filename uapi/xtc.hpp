b86609549eb276f90dd0347d2918c1dd3ca165ca
Michal Kurina <michal.kurina77@proton.me>
2025-04-02 10:38:02 +0000

    Update xtc.hpp

--- uapi/xtc.hpp
+++ uapi/xtc.hpp
@@ -4,25 +4,25 @@
 #include <list>
 
 struct xtc_st {
-	__u16	tabl_ndx;
-	struct {
-		__u8 rex, addr, oper;
-	} pfx;
+    __u16   tabl_ndx;
+    struct {
+        __u8 rex, addr, oper;
+    } pfx;
 
-	struct {
-		OperType	t;
-		__u32		size;
-	} op[4];
-	__u8	op_count;
-	__u8	sc[15];
-	__u8	sc_len;
-	instr *in;
+    struct {
+        OperType    t;
+        __u32       size;
+    } op[4];
+    __u8    op_count;
+    __u8    sc[15];
+    __u8    sc_len;
+    instr *in;
 };
 
 class x_t_c {
 public:
-	x_t_c();
-	__s8 get(__u8 *sc, insn_t& in, __u64 op[4]);
+    x_t_c();
+    __s8 get(__u8 *sc, insn_t& in, __u64 op[4]);
 private:
-	void insert(instr *in);
+    void insert(instr *in);
 };
